b248477df06db27adf544d5c1662ed754391c47f
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2023-04-10 17:10:55 -0500

    returning dataframe from csv

--- funcx/scripts/funcx_infer_maxsep.py
+++ funcx/scripts/funcx_infer_maxsep.py
@@ -8,11 +8,11 @@ def infer_maxsep():
     train_data = "split100"
     test_data = "price"
     infer_maxsep(train_data, test_data, report_metrics=False, pretrained=True)
-    df = pd.read_csv('/results/price_maxsep.csv')
+    df = pd.read_csv('./results/price_maxsep.csv', on_bad_lines='skip')
     print("----- Executed Successfully ----")
     return df
 
-with FuncXExecutor(endpoint_id="ac70db7c-406d-4697-be1e-55cbf56b86f8", container_id="ac70db7c-406d-4697-be1e-55cbf56b86f8") as ex:
+with FuncXExecutor(endpoint_id="006f558d-df82-45c2-b2d5-94274ef41a69", container_id="ac70db7c-406d-4697-be1e-55cbf56b86f8") as ex:
     fut = ex.submit(infer_maxsep)
 
     print(fut.result())
